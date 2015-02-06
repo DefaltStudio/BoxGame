07a8294a94d285f45e7ae1f64d10b4c778d61a31
kvist49 <quist-ch@hotmail.com>
2015-02-06 14:32:23 +0100

    EnemyInvisible
    Usynlighed!!!!!!!

---
+++ Assets/Classes/NotDone/EnemyWanish.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyWanish : MonoBehaviour 
+{
+	public Transform Target;
+	public Color StartColor;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		StartColor = Target.renderer.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnCollisionEnter ()
+	{
+	
+		Target.renderer.material.color = Color.green;
+
+		
+	}
+
+	void OnCollisionExit()
+	{
+		Target.renderer.material.color = StartColor;
+	}
+}
+		    
