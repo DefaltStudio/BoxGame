3418bd30b83bd84a6c6bc2f0b3665266e867b186
Anders W. Olsen <awo@defalt.net>
2015-02-09 17:55:00 +0100

    -Debug.log("...");

--- Assets/Classes/NotDone/ButtonTrigger.cs
+++ Assets/Classes/NotDone/ButtonTrigger.cs
@@ -23,29 +23,22 @@ public class ButtonTrigger : MonoBehaviour {
 
     void OnTriggerEnter(Collider col)
     {
-		Debug.Log ("Trigger");
         if (col.transform.tag == "Player")
         {
-			Debug.Log ("Collider is Player");
             if (trigger.transform.tag == "Door")
             {
-				Debug.Log("Trigger tag is Door");
                 if (doorOpen)
                 {
-					Debug.Log ("Door is open");
 					while(transform != doorStartPosition)
 					{
-						Debug.Log ("Moving.");
 						trigger.transform.position = Vector3.MoveTowards(trigger.transform.position, doorEndPosition.position, doorMoveSpeed * Time.deltaTime);
 					}
 					doorOpen = false;
                 }
                 else if (!doorOpen)
                 {
-					Debug.Log ("Door is closed");
 					while (transform != doorEndPosition)
 					{
-						Debug.Log ("Moving.");
 						trigger.transform.position = Vector3.MoveTowards(trigger.transform.position, doorStartPosition.position, doorMoveSpeed * Time.deltaTime);
 					}
 					doorOpen = true;
