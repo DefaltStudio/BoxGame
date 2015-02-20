001b6115d623d2823ad4c1a68e4aea25a5fc4d24
kvist49 <quist-ch@hotmail.com>
2015-02-20 12:09:32 +0100

    Shooting animation
    Har fået enemyien til kun at animere når player er inrange

---
+++ Assets/Classes/Done/Enemyshoot.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemyshoot : MonoBehaviour {
+
+	Animator anim;
+
+	public float activeRange = 10;
+	
+	static public Transform FPC;
+	public Transform FPS;
+
+	static public bool Open = false;
+
+
+	void Start () 
+		{
+		FPC = FPS;
+			anim = GetComponent<Animator> ();
+		}
+
+	void Update () 
+	{
+		float distanceToPlayer = Vector3.Distance(transform.position, FPC.position);
+	
+
+		if (distanceToPlayer <= activeRange) 
+		{
+			anim.SetBool("Open", true);
+
+			Open = true;
+		}
+
+		if (distanceToPlayer >= activeRange) 
+		{
+			anim.SetBool ("Open", false);
+
+			Open = false;
+		}
+	}
+	
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+
+		Gizmos.DrawWireSphere (transform.position, activeRange);
+	}
+}
---
+++ Assets/Classes/NotDone/EnemyShooting.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShooting : MonoBehaviour 
+{
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		if (Enemyshoot.Open = true) 
+		{
+			Shoot();
+		}
+	}
+
+	void Shoot ()
+	{
+		//Invoke(Shoot,2)
+		transform.LookAt(Enemyshoot.FPC);
+	}
+}
