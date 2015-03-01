67e13f9482abe0a1f91da0bc061f7d9be146c7c5
Anders W. Olsen <awo@defalt.net>
2015-03-01 14:12:25 +0100

    FuckYou, døren virker!

--- Assets/Classes/NotDone/ButtonTrigger.cs
+++ Assets/Classes/NotDone/ButtonTrigger.cs
@@ -3,33 +3,25 @@ using System.Collections;
 
 public class ButtonTrigger : MonoBehaviour
 {
-
     public static bool isTriggered = false;
     public Transform trigger;
 
-    public float doorMoveSpeed;
+    public float doorMoveSpeed = 10f;
     private Vector3 doorStartPosition;
     public Transform doorEndPosition;
     private bool doorOpen = false;
 
-    System.Timers.Timer doorTimer = new System.Timers.Timer();
-
     void Awake()
     {
         doorStartPosition = trigger.transform.position;
-        doorTimer = new System.Timers.Timer(500);
-        doorTimer.Elapsed += OnTimedEvent;
-        doorTimer.Enabled = false;
     }
 
     void Update()
     {
-        //trigger.transform.position = Vector3.MoveTowards(trigger.transform.position, doorEndPosition.transform.position, doorMoveSpeed * Time.deltaTime);
-    }
-
-    IEnumerator wait(float duration)
-    {
-        yield return new WaitForSeconds(duration);
+        if (!doorOpen)
+            trigger.transform.position = Vector3.MoveTowards(trigger.transform.position, doorStartPosition, doorMoveSpeed * Time.deltaTime);
+        else if (doorOpen)
+            trigger.transform.position = Vector3.MoveTowards(trigger.transform.position, doorEndPosition.transform.position, doorMoveSpeed * Time.deltaTime);
     }
 
     void OnTriggerEnter(Collider col)
@@ -38,56 +30,11 @@ public class ButtonTrigger : MonoBehaviour
         {
             if (trigger.transform.tag == "Door")
             {
-                doorTimer.Enabled = true;
-
-                //if (!doorOpen)
-                //{
-                //    while (trigger.transform.position != doorEndPosition.transform.position)
-                //    {
-                //        trigger.transform.position = Vector3.MoveTowards(trigger.transform.position, doorEndPosition.transform.position, doorMoveSpeed);
-                //        wait(1);
-                //    }
-                //    doorOpen = true;
-                //    Debug.Log(doorOpen);
-                //}
-                //else if (doorOpen)
-                //{
-                //    while (trigger.transform.position != doorStartPosition)
-                //    {
-                //        trigger.transform.position = Vector3.MoveTowards(trigger.transform.position, doorStartPosition, doorMoveSpeed);
-                //    }
-                //    doorOpen = false;
-                //    Debug.Log(doorOpen);
-                //}
+                if (doorOpen)
+                    doorOpen = false;
+                else
+                    doorOpen = true;
             }
-        }
-    }
-
-    void OnTimedEvent(object source, System.Timers.ElapsedEventArgs e)
-    {
-        if (!doorOpen)
-        {
-            if (trigger.transform.position != doorEndPosition.transform.position)
-                trigger.transform.position = Vector3.MoveTowards(trigger.transform.position, doorEndPosition.transform.position, doorMoveSpeed);
-            else
-            {
-                doorOpen = true;
-                Debug.Log(doorOpen);
-                this.enabled = false;
-            }
-            
-        }
-        else
-        {
-            if (trigger.transform.position != doorStartPosition)
-                trigger.transform.position = Vector3.MoveTowards(transform.position, doorStartPosition, doorMoveSpeed);
-            else
-            {
-                doorOpen = false;
-                Debug.Log(doorOpen);
-                this.enabled = false;
-            }
-
         }
     }
 }
