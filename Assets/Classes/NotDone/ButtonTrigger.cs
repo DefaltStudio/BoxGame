3c4c2d07158b4fe8adbeff9dbae2b7a761acb6e5
Anders W. Olsen <awo@defalt.net>
2015-03-01 13:29:54 +0100

    Timer

--- Assets/Classes/NotDone/ButtonTrigger.cs
+++ Assets/Classes/NotDone/ButtonTrigger.cs
@@ -12,9 +12,14 @@ public class ButtonTrigger : MonoBehaviour
     public Transform doorEndPosition;
     private bool doorOpen = false;
 
+    System.Timers.Timer doorTimer = new System.Timers.Timer();
+
     void Awake()
     {
         doorStartPosition = trigger.transform.position;
+        doorTimer = new System.Timers.Timer(500);
+        doorTimer.Elapsed += OnTimedEvent;
+        doorTimer.Enabled = false;
     }
 
     void Update()
@@ -33,26 +38,56 @@ public class ButtonTrigger : MonoBehaviour
         {
             if (trigger.transform.tag == "Door")
             {
-                if (!doorOpen)
-                {
-                    while (trigger.transform.position != doorEndPosition.transform.position)
-                    {
-                        trigger.transform.position = Vector3.MoveTowards(trigger.transform.position, doorEndPosition.transform.position, doorMoveSpeed);
-                        wait(1);
-                    }
-                    doorOpen = true;
-                    Debug.Log(doorOpen);
-                }
-                else if (doorOpen)
-                {
-                    while (trigger.transform.position != doorStartPosition)
-                    {
-                        trigger.transform.position = Vector3.MoveTowards(trigger.transform.position, doorStartPosition, doorMoveSpeed);
-                    }
-                    doorOpen = false;
-                    Debug.Log(doorOpen);
-                }
+                doorTimer.Enabled = true;
+
+                //if (!doorOpen)
+                //{
+                //    while (trigger.transform.position != doorEndPosition.transform.position)
+                //    {
+                //        trigger.transform.position = Vector3.MoveTowards(trigger.transform.position, doorEndPosition.transform.position, doorMoveSpeed);
+                //        wait(1);
+                //    }
+                //    doorOpen = true;
+                //    Debug.Log(doorOpen);
+                //}
+                //else if (doorOpen)
+                //{
+                //    while (trigger.transform.position != doorStartPosition)
+                //    {
+                //        trigger.transform.position = Vector3.MoveTowards(trigger.transform.position, doorStartPosition, doorMoveSpeed);
+                //    }
+                //    doorOpen = false;
+                //    Debug.Log(doorOpen);
+                //}
             }
+        }
+    }
+
+    void OnTimedEvent(object source, System.Timers.ElapsedEventArgs e)
+    {
+        if (!doorOpen)
+        {
+            if (trigger.transform.position != doorEndPosition.transform.position)
+                trigger.transform.position = Vector3.MoveTowards(trigger.transform.position, doorEndPosition.transform.position, doorMoveSpeed);
+            else
+            {
+                doorOpen = true;
+                Debug.Log(doorOpen);
+                this.enabled = false;
+            }
+            
+        }
+        else
+        {
+            if (trigger.transform.position != doorStartPosition)
+                trigger.transform.position = Vector3.MoveTowards(transform.position, doorStartPosition, doorMoveSpeed);
+            else
+            {
+                doorOpen = false;
+                Debug.Log(doorOpen);
+                this.enabled = false;
+            }
+
         }
     }
 }
