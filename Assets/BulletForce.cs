d0d238c663084c4f223b7e9e165be52d8110ee3d
kvist49 <quist-ch@hotmail.com>
2015-06-22 12:06:57 +0200

    EnemyShoot

---
+++ Assets/BulletForce.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletForce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * 10000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Classes/NotDone/EnemyShooting.cs
+++ Assets/Classes/NotDone/EnemyShooting.cs
@@ -4,22 +4,30 @@ using System.Collections;
 public class EnemyShooting : MonoBehaviour 
 {
 	public Transform FPC;
+	public Transform Spawn;
+	public Transform Bullet;
 	public float RotationSpeed;
 	public float ActiveRange;
+	public float Temprotation = 0.0f;
+	public float fireRate = 0.5f;
+
+	private float nextFire = 0.0f;
 
 	private Vector3 direction;
 	private Quaternion lookRotation;
+
 	void Start () 
 	{
-	
+		
 	}
 
 	void Update () 
 	{
 
+		
 		//GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
 
-	/*	//Find hvor langt FPC (First person Controller) er væk
+		//Find hvor langt FPC (First person Controller) er væk
 		float DistanceToTarget = Vector3.Distance(FPC.position, transform.position);
 
 
@@ -30,20 +38,26 @@ public class EnemyShooting : MonoBehaviour
 		lookRotation = Quaternion.LookRotation (direction);
 
 		//kig på target hvis den er inden for range
-*/
+		
 		if (DistanceToTarget < ActiveRange) 
 		{
-			transform.LookAt (FPC);
-			//transform.rotation = Quaternion.Slerp (transform.rotation, lookRotation, Time.deltaTime * RotationSpeed);
+			Shoot();
 		}
-			//Shoot();
+
 
 	
 	}
 
 	void Shoot ()
 	{
-		//Invoke(Shoot,2)
-		//transform.LookAt(FPC);
+
+		transform.rotation = Quaternion.Slerp (transform.rotation,lookRotation, Time.deltaTime * RotationSpeed);
+		if (Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate (Bullet, Spawn.position, Spawn.rotation);
+
+		}
+	
 	}
 }
