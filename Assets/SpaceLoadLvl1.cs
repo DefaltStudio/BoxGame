867ae84b1576351a7b271a6325276a8f4835a42c
Anders W. Olsen <awo@defalt.net>
2015-07-05 23:12:58 +0200

    Start Menu Level

---
+++ Assets/SpaceLoadLvl1.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceLoadLvl1 : MonoBehaviour {
+
+	void Update () {
+        if (Input.GetButtonDown("StartGame"))
+            Application.LoadLevel(2);
+	}
+}
