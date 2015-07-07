6c364fe05a6d141dbab8fde893ad212d9d0ab07f
Anders W. Olsen <awo@defalt.net>
2015-07-07 19:43:55 +0200

    End Message Auto-exit

---
+++ Assets/EndMessageExitGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndMessageExitGame : MonoBehaviour {
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(30);
+        if (!Application.isEditor)
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+    }
+}
