7781496e3588b598dbf7ba3a27c12f2eeda02051
Anders Wiberg Olsen <andersvvolsen@gmail.com>
2015-10-26 12:34:18 +0100

    Global Illumination, Lights, Reflection Probes

--- Assets/Classes/Done/Pickups/GoldCube.cs
+++ Assets/Classes/Done/Pickups/GoldCube.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class GoldCube : MonoBehaviour 
+public class GoldCube : MonoBehaviour
 {
-	public GameObject GoldExplosion;
+    public GameObject GoldExplosion;
+    public float duration = 1.0f;
     private Vector3 goldCubeStartPos;
 
     void Start()
     {
         Manager.goldCubes.Add(gameObject);
         goldCubeStartPos = transform.position;
+    }
+
+    void Update()
+    {
+        float phi = Time.time / duration * 2 * Mathf.PI;
+        float amplitude = Mathf.Cos(phi) * 0.5f + 2f;
+        GetComponentInChildren<Light>().intensity = amplitude;
     }
 
     void OnTriggerEnter(Collider col)
@@ -19,7 +26,7 @@ public class GoldCube : MonoBehaviour
             Manager.goldCubeStartLocations.Add(goldCubeStartPos);
             Debug.Log("Gold cube collected!");
             Manager.goldCubes.Remove(gameObject);
-			Instantiate(GoldExplosion, transform.position, transform.rotation);
+            Instantiate(GoldExplosion, transform.position, transform.rotation);
             Destroy(gameObject);
         }
     }
